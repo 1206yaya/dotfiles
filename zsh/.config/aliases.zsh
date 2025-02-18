1b83f421c43ca0d3828d0b94fce5c56b43f61007
1206yaya <1206yaya@gmail.com>
2025-02-18 09:02:58 +0900

    fix: create_symbolic_links

--- zsh/.config/aliases.zsh
+++ zsh/.config/aliases.zsh
@@ -1,4 +1,3 @@
-
 export GIT_CLONE_PATH=~/ghq/github.com/1206yaya
 export DOTDIR="$GIT_CLONE_PATH/dotfiles"
 
@@ -12,7 +11,17 @@ alias localip="ifconfig en0 | grep 'inet ' | awk '{ print $2 }'"
 alias ls="ls -lt  "$@""
 alias pb="pbcopy"
 alias mkdir='mkdir -p' # 深いネストのディレクトリも作成可能
-function touchp() { if [[ "$1" == */* ]]; then if [ ! -d "${1%/*}/" ]; then mkdir -p "${1%/*}/"; fi; fi; touch "$1" } # ディレクトリがない場合は作成してファイルを作成
+# ディレクトリがない場合は作成してファイルを作成
+function touchp() {
+  if [[ "$1" == */* ]]; then
+    dir="${1%/*}"
+    if [ ! -d "$dir" ]; then
+      mkdir -p "$dir"
+    fi
+  fi
+  touch "$1"
+}
+
 alias genb="git switch -c "$@""
 alias ghclose="gh issue close "$@""
 alias ghcreate='gh issue create --title "$@" --body "Issue description"'
@@ -27,29 +36,27 @@ alias cdd="builtin cd"
 # デフォルトは 10
 function genpass() {
   local length=${1:-10}
-  pwgen -1  -B -c -n $length 1
+  pwgen -1 -B -c -n $length 1
   # 特殊記号: -s -y
 }
 alias aopa='~/bin/toggle_alacritty_opacity.sh'
-
 
 # タイムスタンプ順にソート (最新ファイルを上に)
 alias lt="eza -l --sort changed"
 alias ld='eza -D'
 alias la='eza -la'
 if [ -n "$(which eza)" ]; then
-    alias ls="eza -l --sort changed"
+  alias ls="eza -la --sort changed"
 fi
 ezt() {
   eza --tree "$@"
 }
 
-
 function pwd() {
   builtin pwd | tee >(pbcopy)
 }
 function man() {
-    env LESS_TERMCAP_mb=$'\E[01;31m' \
+  env LESS_TERMCAP_mb=$'\E[01;31m' \
     LESS_TERMCAP_md=$'\E[01;38;5;74m' \
     LESS_TERMCAP_me=$'\E[0m' \
     LESS_TERMCAP_se=$'\E[0m' \
@@ -59,27 +66,24 @@ function man() {
     man "$@"
 }
 
-
-
 function mvp() {
-    target_dir=$(dirname "$2")
-    if [ ! -d "$target_dir" ]; then
-        mkdir -p "$target_dir"
-    fi
-    mv "$1" "$2"
+  target_dir=$(dirname "$2")
+  if [ ! -d "$target_dir" ]; then
+    mkdir -p "$target_dir"
+  fi
+  mv "$1" "$2"
 }
 function cpp() {
-    # Usage: cpp <source_file> <destination_directory>
-    # 指定したファイルをコピーし、コピー先ディレクトリが存在しない場合は自動作成する
-    # 注意点:
-    # - コピー先にファイルを指定すると意図しない動作をする可能性がある (ディレクトリを指定すること)
+  # Usage: cpp <source_file> <destination_directory>
+  # 指定したファイルをコピーし、コピー先ディレクトリが存在しない場合は自動作成する
+  # 注意点:
+  # - コピー先にファイルを指定すると意図しない動作をする可能性がある (ディレクトリを指定すること)
 
-    if [ ! -d "$2" ]; then
-        mkdir -p "$2"
-    fi
-    cp "$1" "$2"
+  if [ ! -d "$2" ]; then
+    mkdir -p "$2"
+  fi
+  cp "$1" "$2"
 }
-
 
 alias cat='bat --style=plain --paging=never'
 alias less='bat --style=plain'
@@ -115,7 +119,7 @@ alias pes="pet sync"
 #　直前のコマンドをpet に登録
 function prev() {
   PREV=$(fc -lrn | head -n 1)
-  sh -c "pet new `printf %q "$PREV"`"
+  sh -c "pet new $(printf %q "$PREV")"
 }
 function open() {
   if [[ $@ == "pdf" ]]; then
@@ -128,7 +132,7 @@ function open() {
 function terminal() {
   CURRENT_DIR=$(pwd)
 
-osascript <<END
+  osascript <<END
 tell application "iTerm"
     activate
     try
@@ -147,23 +151,19 @@ end tell
 END
 }
 
-
-
-
 pulsar_open() {
-    if [ $# -eq 0 ]; then
-        # 引数がない場合は現在のディレクトリを開く
-        open -na 'Pulsar.app' --args "$PWD"
-    else
-        # 引数がある場合はそのパスを絶対パスに変換して開く
-        open -na 'Pulsar.app' --args "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
-    fi
+  if [ $# -eq 0 ]; then
+    # 引数がない場合は現在のディレクトリを開く
+    open -na 'Pulsar.app' --args "$PWD"
+  else
+    # 引数がある場合はそのパスを絶対パスに変換して開く
+    open -na 'Pulsar.app' --args "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
+  fi
 }
 
 alias pulsar=pulsar_open
 
-
-function npminstall (){
+function npminstall() {
   npm install -g firebase-tools
   npm install -g tsc
   npm install -g typesync
@@ -196,25 +196,21 @@ function hub() {
   fi
 }
 
-
-
 ### クリップボードの内容を特定のフォルダにタイムスタンプ付きでファイルとして保存する
 function cbf() {
-  
+
   FOLDER_PATH=~/Downloads
 
   # 現在のタイムスタンプを取得します。
   TIMESTAMP=$(date "+%Y%m%d%H%M%S")
 
   # クリップボードの内容をファイルに保存します。
-  pbpaste > "$FOLDER_PATH/$TIMESTAMP.txt"
+  pbpaste >"$FOLDER_PATH/$TIMESTAMP.txt"
 }
-
 
 grep() {
   command grep --color -E "$@"
 }
-
 
 function tmpdir() {
   NOW=$(date "+%Y-%m-%d%H%M")
@@ -223,12 +219,12 @@ function tmpdir() {
   if [ ! -d "$TMP_DIR" ]; then
     mkdir -p "$TMP_DIR"
   fi
-  builtin cd ${TMP_DIR}; code .
+  builtin cd ${TMP_DIR}
+  code .
 }
 
-
 function mkcd() {
-    mkdir -p "$@" && cd "$_";
+  mkdir -p "$@" && cd "$_"
 }
 
 #* ChromeのWindowタイトルに特定の文字列が含まれているもののみを終了するということはできない
@@ -244,20 +240,17 @@ function chat() {
 }
 # killPort <port>
 function killport() {
-    if [[ -n "$1" ]]
-    then
-        lsof -t -i tcp:"$1" | xargs kill
-    else
-        echo 'Error: please provide a port number.'
-    fi
+  if [[ -n "$1" ]]; then
+    lsof -t -i tcp:"$1" | xargs kill
+  else
+    echo 'Error: please provide a port number.'
+  fi
 }
 
 # この関数は、コマンド履歴の一覧を表示し、pecoで選択したコマンドを実行するものです。
 function peco-history-pbcopy() {
-  history -n 1 | tail -r  | awk '!a[$0]++' | peco --layout=bottom-up | tr -d "\r\n" | pbcopy 
+  history -n 1 | tail -r | awk '!a[$0]++' | peco --layout=bottom-up | tr -d "\r\n" | pbcopy
 }
 # Control + hh で実行
 zle -N peco-history-pbcopy
 bindkey '^H^H' peco-history-pbcopy
-
-
